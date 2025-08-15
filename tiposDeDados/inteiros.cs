85c1cd073916f504bd8b4588a4a83a0e27429c1f
Matheus Calmon <sanderhus15@outlook.com>
2025-08-14 22:04:40 -0300

    first commit

--- tiposDeDados/inteiros.cs
+++ tiposDeDados/inteiros.cs
@@ -6,9 +6,6 @@ namespace MeuApp
     {
         static void Main(string[] args)
         {
-
-            // Temos 3 tipos de dados para representar numeros inteiros sendo o mais comun e utlizado como padrão VAR o INT
-
             short numeroShort = 10;
             int numeroInt = 123;
             long numeroLong = 12312323;
--- tiposDeDados/var.cs
+++ tiposDeDados/var.cs
@@ -6,8 +6,6 @@ namespace MeuApp
     {
         static void Main(string[] args)
         {
-            // O var seleciona o tipo de dado que melhor se enquadra
-
             var numero1 = 1;
             var numero2 = 2.5;
             var unicoCaracter = 'A';
