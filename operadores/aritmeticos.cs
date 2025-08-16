c7165de28663ec5d81f61b9e34be284a3acb606d
Matheus Calmon <sanderhus15@outlook.com>
2025-08-16 11:21:52 -0300

    operadores

---
+++ operadores/aritmeticos.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Multiplicaçao e divisão são executadas primeiros se quiser
+            // mudar a order, usar parenteses
+
+            //soma
+            1 + 1;
+
+            // subtraçào
+            2 - 1;
+
+            //multiplicação
+            2 * 2;
+
+            //Divisão
+            4 / 2;
+        }
+    }
+}
