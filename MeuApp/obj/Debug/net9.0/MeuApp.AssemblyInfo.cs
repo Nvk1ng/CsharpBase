643d1e1c7e5c8e14530c7fae44a895382963825f
Matheus Calmon <sanderhus15@outlook.com>
2025-08-16 11:10:26 -0300

    tiposDeDados

--- MeuApp/obj/Debug/net9.0/MeuApp.AssemblyInfo.cs
+++ MeuApp/obj/Debug/net9.0/MeuApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeuApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85c1cd073916f504bd8b4588a4a83a0e27429c1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+079132e5e9bb62f6f1fb9a16549cb080c0f643d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeuApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeuApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
